97d288a7a1695b45320d6b1098b677c7c6bc4e0a
ren-zaifei <chuen6@qq.com>
2026-08-23 10:04:29 +0800

    init

---
+++ src/main/java/com/renzaifei/anvilcraftappliedgrid/AnvilCraftAppliedGrid.java
@@ -0,0 +1,19 @@
+package com.renzaifei.anvilcraftappliedgrid;
+
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.ModContainer;
+
+@Mod(AnvilCraftAppliedGrid.MODID)
+public class AnvilCraftAppliedGrid {
+    public static final String MODID = "anvilcraftappliedgrid";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public AnvilCraftAppliedGrid(IEventBus modEventBus, ModContainer modContainer) {
+
+    }
+}
